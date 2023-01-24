06c51d7e452e159c0a6789a1880722ff257650eb
amolojr <97804873+amolojr@users.noreply.github.com>
2023-01-24 07:19:18 +0300

    Add files via upload

---
+++ python program to calculate volume of a sphere importing math module using radius.py3.py
@@ -0,0 +1,7 @@
+import math
+
+# input radius of sphere
+radius = float(input("Enter the radius of the sphere: "))
+
+volume = (4/3) * math.pi * (radius**3)
+print("The volume of the sphere is", volume)
