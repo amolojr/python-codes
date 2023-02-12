b15a1ea11cf3dca6188d2fa28ebfe7f4f24b160f
amolojr <97804873+amolojr@users.noreply.github.com>
2023-02-12 17:03:35 +0300

    Add files via upload

---
+++ #python program to prompt the largest of.py
@@ -0,0 +1,10 @@
+#python program to prompt the largest of the three numbers
+a=int(input("Enter number a"))
+b=int(input("Enter number b"))
+c=int(input("Enter number c"))
+if(a>b and a>c):
+    print("a is the largest number")
+elif(b>a and b>c):
+    print("b is the largest numnber")
+else:
+    ("print c is the largest number")    
