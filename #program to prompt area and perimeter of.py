6fd1856c6fad3910983c50148eb09e27b92b2cb8
amolojr <97804873+amolojr@users.noreply.github.com>
2023-03-07 06:14:08 +0300

    Add files via upload

---
+++ #program to prompt area and perimeter of.py
@@ -0,0 +1,16 @@
+#program to prompt area and perimeter of a circle
+class circle:
+    def __init__(self,radius):
+     self.radius=radius
+
+def area(self):
+    a=3.142*self.radius**2
+    return a
+
+def perimeter(self):
+    p=3.142*self.radius*2
+    return p
+
+c=circle(5)
+print(circle.area())
+print(c.perimeter())
